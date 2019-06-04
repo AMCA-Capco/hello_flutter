46da6d62fd942c89fd26a40f2f768560dae4756b
McCarroll, Alexandra <Alexandra.McCarroll@capco.com>
2019-06-04 14:19:34 +0100

    Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
+import './screens/home.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: "Hello Flutter App",
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(appBar: AppBar(
-        title: Text("Title in App Bar"),
-      ),
-      body: Material(
-        color: Colors.deepPurple,
-        child: Center(
-          child: Text(
-            "Hello Flutter!",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white, fontSize: 36.0),
-          ),
+// Entry point
+void main() => runApp(new HelloFlutterApp());
+// Only use fat arrow functions in 1 line functions
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Hello Flutter App",
+        home: Scaffold(
+          appBar: AppBar(
+          title: Text("Title in App Bar"),
         ),
-      ),
-      ),
-    )
-    
-  );
+        body: Home()
+        ),
+      );
+  }
 }
 
 // everything is a widget - Center; Text
 // ltr (left to right)
+// StatelessWidget = Super class & HelloFltterApp = sub-class
